769f1b5db1bee00b689d19269b4639be707f5e0b
Vatsal Salla <sallavatsal@gmail.com>
2018-06-02 14:41:59 -0500

    Updated

--- pyramidUsingAlphabets.c
+++ pyramidUsingAlphabets.c
@@ -1,44 +1,23 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="cProjects" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/cProjects" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/cProjects" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-		</Compiler>
-		<Unit filename="../cProjects/main.c">
-			<Option compilerVar="CC" />
-		</Unit>
-		<Extensions>
-			<envvars />
-			<code_completion />
-			<lib_finder disable_auto="1" />
-			<debugger />
-		</Extensions>
-	</Project>
-</CodeBlocks_project_file>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x, y, z;
+
+    char alphabet;
+
+    printf("\nEnter a capital alphabet:");
+    scanf("%c", &alphabet);
+
+    z = alphabet;                           //Storing Ascii
+
+    for( x = 'A' ; x <= alphabet ; x++ ){
+        for( y = x; y >= 'A' ; y-- ){
+            printf(" %c ", x);
+        }
+
+        printf("\n");
+
+    }P
+}
